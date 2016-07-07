c2b0bd5e2234e4145bc3b18d5be2f6727387b7e5
Chris Heller <ctheller12@gmail.com>
2016-07-07 17:56:17 -0400

    removed hashtag from urls

--- browser/js/states.js
+++ browser/js/states.js
@@ -1,5 +1,4 @@
-juke.config(function($stateProvider, $urlRouterProvider){
-
+juke.config(function($stateProvider, $urlRouterProvider, $locationProvider){
 
 	$urlRouterProvider.when('/', '/albums');
 	$urlRouterProvider.when('', '/albums');
@@ -54,5 +53,19 @@ juke.config(function($stateProvider, $urlRouterProvider){
 			url: "/songs",
 			templateUrl: '/js/artist/artist.songs.template.html'
 		})
+
+		//check browser support
+    if(window.history && window.history.pushState){
+        //$locationProvider.html5Mode(true); will cause an error $location in HTML5 mode requires a  tag to be present! Unless you set baseUrl tag after head tag like so: <head> <base href="/">
+
+     // to know more about setting base URL visit: https://docs.angularjs.org/error/$location/nobase
+
+     // if you don't wish to set base URL then use this
+     $locationProvider.html5Mode({
+             enabled: true,
+             requireBase: false
+      });
+   }
+
 })
 
