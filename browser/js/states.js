db0cd2f0dd3f3556a3ff22881f9425250cedae9b
Chris Heller <ctheller12@gmail.com>
2016-07-07 17:10:51 -0400

    changed app to use Resolve for ajax requests

--- browser/js/states.js
+++ browser/js/states.js
@@ -3,21 +3,41 @@ juke.config(function($stateProvider){
 		.state('allAlbums', {
 			url: "/albums",
 			templateUrl: '/js/album/allAlbums.template.html',
+			resolve: {
+				albums: function(AlbumFactory){
+					return AlbumFactory.fetchAll();
+				}
+			},
 			controller: 'AlbumsCtrl'
 		})
 		.state('allArtists', {
 			url: "/artists",
 			templateUrl: '/js/artist/allArtists.template.html',
+			resolve: {
+				artists: function(ArtistFactory){
+					return ArtistFactory.fetchAll();
+				}
+			},
 			controller: "ArtistsCtrl"
 		})
 		.state('album', {
 			url: "/album/:id",
 			templateUrl: '/js/album/album.template.html',
+			resolve: {
+				album: function(AlbumFactory, $stateParams){
+					return AlbumFactory.fetchById($stateParams.id);
+				}
+			},
 			controller: "AlbumCtrl"
 		})
 		.state('artist', {
 			url: "/artist/:id",
 			templateUrl: '/js/artist/artist.template.html',
+			resolve: {
+				artist: function(ArtistFactory, $stateParams){
+					return ArtistFactory.fetchById($stateParams.id);
+				}
+			},
 			controller: "ArtistCtrl"
 		})
 		.state('artist.albums', {
